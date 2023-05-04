dc28de15248ca23326caece1c7803b0858199638
niknik3610 <niklas.harnish@hotmail.com>
2023-05-04 09:47:02 +0200

    draws a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use beryllium::{
+    *, 
+    init::InitFlags, events::Event
+};
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = Sdl::init(InitFlags::EVERYTHING);
+    sdl.set_gl_context_major_version(3).expect("Failed Setting Major Version");
+    sdl.set_gl_context_minor_version(3).expect("Failed Setting Minor Version");
+    sdl.set_gl_profile(video::GlProfile::Core).expect("Failed Setting OpenGL to Core");
+
+    let window = sdl.create_gl_window(
+            video::CreateWinArgs { 
+                title: "Hello World", 
+                width: 400, 
+                height: 400, 
+                allow_high_dpi: true, 
+                borderless: false, 
+                resizable: false 
+            }             
+        ).expect("Failed to Create Window");
+
+    'event_loop: loop {
+        while let Some(event) = sdl.poll_events() {
+            match event {
+                (Event::Quit, _) => break 'event_loop,
+                (_, a) => println!("TimeStamp: {a}"),
+            }
+        }
+    }
 }
