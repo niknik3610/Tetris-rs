a977beb03dbebc5185060fdfbcc4b6a26d991952
niknik3610 <niklas.harnish@hotmail.com>
2023-05-06 13:42:31 +0200

    more progress on open gl functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use sdl2::event::Event as SdlEvent;
+mod shader;
+use sdl2::{event::Event as SdlEvent, video::gl_attr};
+use std::ffi::{CStr, CString};
+use gl::types::GLuint;
+
 fn main() {
     let sdl = sdl2::init().expect("Failed to init SDL");    
     let video_subsystem = sdl.video().expect("Failed to init video");
-    let window = video_subsystem.window(
-        "Tetris", 
-        800, 
-        400 
-        )
+    
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4,5);
+
+    let window = video_subsystem
+        .window("Tetris", 800, 400)
+        .opengl()
         .build()
         .expect("Failed to start window");
+    let gl_context = window.gl_create_context().expect("Failed to create Context");
+    let gl = gl::load_with (|f|
+        video_subsystem.gl_get_proc_address(f) as *const std::os::raw::c_void
+        );
+    
+    unsafe { 
+        gl::Viewport(0, 0, 800, 400);
+        gl::ClearColor(0.0, 0.5, 1.0, 1.0);
+    }
     let mut event_pump = sdl.event_pump().expect("Failed to init event pump");
     'run_loop: loop {
         for event in event_pump.poll_iter() {
@@ -17,5 +33,10 @@ fn main() {
                 _ => {}
             }
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.gl_swap_window();
     }
 }
---
+++ src/shader.rs
@@ -0,0 +1,71 @@
+use crate::{
+    CStr,
+    GLuint,
+    CString
+};
+
+pub struct Shader {
+    pub id: GLuint,
+}
+impl Shader {
+    fn from_source(
+        source: &CStr, 
+        kind: GLuint
+        ) -> Result<Shader, String> {
+        let id = shader_from_string(source, kind)?;
+        Ok(Shader{id})
+    }
+    fn from_vert_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_string(source, gl::VERTEX_SHADER)?;
+        return Ok(Shader{id});
+    }
+    fn from_frag_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_string(source, gl::FRAGMENT_SHADER)?;
+        return Ok(Shader{id});
+    }
+}
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteShader(self.id);
+        }
+    }
+}
+
+fn shader_from_string(
+    source: &CStr,
+    kind: GLuint
+    ) -> Result<GLuint, String> {
+    let id = unsafe {   gl::CreateShader(kind) };
+
+    let mut success = 1; 
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id);
+        gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == 0 {
+        let mut err_len = 0;
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut err_len);
+        }
+        let buffer = create_empty_cstring(err_len as usize);
+        unsafe {
+            gl::GetShaderInfoLog (
+                id,
+                err_len + 1,
+                std::ptr::null_mut(),
+                buffer.as_ptr() as *mut gl::types::GLchar
+                );
+        }
+        return Err(buffer.to_string_lossy().into_owned());
+    }
+    return Ok(id);
+}
+
+fn create_empty_cstring(len: usize) -> CString {
+    let mut buffer: Vec<u8> = Vec::with_capacity(len+1);
+    buffer.extend([b' '].iter().cycle().take(len));
+    unsafe { CString::from_vec_unchecked(buffer) } 
+}
