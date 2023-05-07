b82cab0078c91d44d484151b977823ff95c18640
niknik3610 <niklas.harnish@hotmail.com>
2023-05-07 12:40:08 +0200

    fixed the triangle :|

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod shader;
 mod program;
-use sdl2::{event::Event as SdlEvent, video::gl_attr};
+use sdl2::event::Event as SdlEvent;
 use std::ffi::{CStr, CString};
 use gl::types::GLuint;
 
@@ -10,22 +10,18 @@ fn main() {
     
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(4,5);
+    gl_attr.set_context_version(4, 5);
 
     let window = video_subsystem
-        .window("Tetris", 960, 590)
+        .window("Tetris", 900, 700)
         .opengl()
         .build()
         .expect("Failed to start window");
-    let gl_context = window.gl_create_context().expect("Failed to create Context");
-    let gl = gl::load_with (|f|
-        video_subsystem.gl_get_proc_address(f) as *const std::os::raw::c_void
-        );
     
-    unsafe { 
-        gl::Viewport(0, 0, 960, 590);
-        gl::ClearColor(0.0, 0.5, 1.0, 1.0);
-    }
+    let _gl_context = window.gl_create_context().expect("Failed to create Context");
+    let _gl = gl::load_with (|f|
+        video_subsystem.gl_get_proc_address(f) as *const std::os::raw::c_void
+        ); 
 
     let vert_shader = shader::Shader::from_vert_source(
         &CString::new(include_str!("triangle.vert")).unwrap()
@@ -38,16 +34,15 @@ fn main() {
     let shader_program = program::Program::from_shaders(
         &[vert_shader, frag_shader]
         ).unwrap();
-    shader_program.set_used();
 
-    let vertices = vec![
-        -0.5, -0.5, 0.0,
+
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0, 
         0.5, -0.5, 0.0,
         0.0, 0.5, 0.0
     ];
-
     //vertex buffer object
-    let mut vbo = 0;
+    let mut vbo: GLuint = 0;
     unsafe {
         gl::GenBuffers(1, &mut vbo);
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
@@ -81,6 +76,11 @@ fn main() {
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
         gl::BindVertexArray(0);
     }
+    
+    unsafe { 
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);    
+    }
 
     let mut event_pump = sdl.event_pump().expect("Failed to init event pump");
     'run_loop: loop {
@@ -90,16 +90,18 @@ fn main() {
                 _ => {}
             }
         }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
         shader_program.set_used();
         unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::BindVertexArray(vao);
             gl::DrawArrays(
                 gl::TRIANGLES,  //mode
                 0,  //start index of array
                 3   //number of indexes
-                )
+                );
         }
         window.gl_swap_window();
     }
