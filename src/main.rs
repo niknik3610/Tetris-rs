e8fc3109d2ef6ccd9cb0bf4b82a7c66577322d18
niknik3610 <niklas.harnish@hotmail.com>
2023-05-09 14:02:22 +0200

    idk

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use opengl_backend::VideoBuffer;
 use sdl2::event::Event as SdlEvent;
 
 pub const RESOLUTION: (u32, u32) = (800, 800);
-pub const GRID_SIZE: (u32, u32) = (80, 80);
+pub const GRID_SIZE: (u32, u32) = (RESOLUTION.0/10, RESOLUTION.1/10);
 pub const SIZE_MULTIPLIER: u32 = 4;
 pub const QUAD_SIZE: f32 = (RESOLUTION.0 / GRID_SIZE.0 * SIZE_MULTIPLIER) as f32;
 
@@ -20,12 +20,10 @@ pub const GRID_END: [u32; 2] = [
         20
 ];
 
-
-
 fn main() {
     let mut gl_context = opengl_backend::init_sdl().unwrap();
     let mut video_buffer = VideoBuffer::new();
-    create_bg(&mut video_buffer);
+    draw_board(&mut video_buffer);
     
     let bglen = video_buffer.bg_verts.len() / 6;
     'run_loop: loop {
@@ -46,7 +44,7 @@ fn main() {
     }
 }
 
-fn create_bg(video_buffer: &mut VideoBuffer) {
+fn draw_board(video_buffer: &mut VideoBuffer) {
     let color_one = (115, 34, 122);
     let color_two = (87, 12, 94);
 
